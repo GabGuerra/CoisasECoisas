1bf3356b7cad8f789a869ca3cc87202fa97290fa
Alexandre Borges <borges_2.0@hotmail.com>
2019-07-09 13:46:39 -0300

    #Commit - Cliente e Produtos com barra de pesquisa!

---
+++ ProvaOO2/Controllers/ProdutoesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProvaOO.Models;
+using System.Threading.Tasks;
+
+namespace ProvaOO2.Controllers
+{
+    public class ProdutoesController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Produtos por ID
+        public async Task<ActionResult> Index(string nome)
+        {
+            var produto = from p in db.produto
+                          select p;
+            if (!String.IsNullOrEmpty(nome))
+            {
+                produto = produto.Where(p => p.nome.Contains(nome));
+            }
+
+            return View(await produto.ToListAsync());
+        }
+
+        // GET: Produtoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: Produtoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produtoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "produtoId,nome,preco,descricao,openbox")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.produto.Add(produto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(produto);
+        }
+
+        // GET: Produtoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produtoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "produtoId,nome,preco,descricao,openbox")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(produto);
+        }
+
+        // GET: Produtoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produtoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Produto produto = db.produto.Find(id);
+            db.produto.Remove(produto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
